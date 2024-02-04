a65895b3b36fef1819df22fc43cbd5dbd9737451
Magela_Cornelio <gelacornelio3@gmail.com>
2024-02-04 18:21:20 -0500

    CP_1 completed

---
+++ CP_1/Program.cs
@@ -0,0 +1,50 @@
+﻿//ejercicio 1
+//System.Console.WriteLine("Hello World");
+
+//ejercicio 2 
+//System.Console.WriteLine(int.MinValue);
+//System.Console.WriteLine(int.MaxValue);
+
+//ejercicio 3 (pi)
+//System.Console.WriteLine(Math.PI);
+
+//ejercicio 4 (string + Medio limon)
+/*System.Console.WriteLine("escribe una palabra");
+string entrada = Console.ReadLine();
+entrada += " medio limon";
+System.Console.WriteLine(entrada);*/
+
+//ejercicio 5 (doble de la entrada)
+//System.Console.WriteLine("escribe un numero");
+//int a = Console.ReadLine();
+//System.Console.WriteLine(2 * a);
+
+//ejercicio 6 (pormedio y valor medio)
+/*System.Console.WriteLine("escribe tres numeros");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+int promedio = (a + b + c) / 3;
+int max1 = Math.Max(a, b);
+int max2 = Math.Max(b, c);
+int valor_medio = Math.Min(max1, max2);
+System.Console.WriteLine("El valor medio es " + valor_medio);
+System.Console.WriteLine("El promedio es " + promedio);*/
+
+// ejercicio 7 (ecuacion cuadratica)
+/*System.Console.WriteLine("escribe 3 numeros");
+double a = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+double c = double.Parse(Console.ReadLine());
+double s1 = (-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / 2 * a;
+double s2 = (-b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / 2 * a;
+System.Console.WriteLine($"{s1} + {s2}");*/
+
+
+//ejercicio 8 (Fecha de naciemiento)
+/*System.Console.WriteLine("escriba su numerdo de identidad");
+long I = long.Parse(Console.ReadLine());
+long año = (I / 1000000000) % 100;
+long mes = (I / 10000000) % 100;
+long dia = (I / 100000) % 100;
+System.Console.WriteLine($"{dia}/{mes}/200{año}");*/
